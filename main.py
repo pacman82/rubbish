f7cf30c550dc9da1f5b9ac4be380a6bc49015019
Markus Klein <markus-klein@live.de>
2025-11-07 18:07:02 +0100

    feat: Train bigram model

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import final
+from typing import final, override
 
 import torch
 import torch.nn as nn
@@ -54,20 +54,38 @@ class Data:
 class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size: int):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table: nn.Embedding = nn.Embedding(vocab_size, vocab_size)
 
-    def forward(self, idx: Tensor, targets: Tensor):
+    @override
+    def forward(
+        self, idx: Tensor, targets: Tensor | None = None
+    ) -> tuple[Tensor, Tensor | None]:
         logits = self.token_embedding_table(idx)
-        # Logits have dimension (Batch, Time, Channel)
-        B, T, C = logits.shape
-        logits = logits.view(B * T, C)
-        # Targets has have dimension (Batch, Time)
-        B, T = targets.shape
-        targets = targets.view(B * T)
 
-        loss = nn.functional.cross_entropy(logits, targets)
+        if targets is not None:
+            # Optionally calculate loss and reduce dimension of logits
+
+            # Logits have dimension (Batch, Time, Channel)
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            # Targets has have dimension (Batch, Time)
+            targets = targets.view(B * T)
+            loss = nn.functional.cross_entropy(logits, targets)
+        else:
+            loss = None
 
         return logits, loss
+
+    def generate(self, idx: Tensor, max_new_tokens: int) -> Tensor:
+        print(idx.shape)
+        print(idx)
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:, -1, :]
+            probs = nn.functional.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
 
 
 def main():
@@ -78,16 +96,29 @@ def main():
 
     tokenizer = Tokenizer(text)
     context_size = 8
-    batch_size = 4
+    batch_size = 32
     data = Data(text, tokenizer, context_size, batch_size)
-
-    xb, yb = data.training_batch()
 
     model = BigramLanguageModel(tokenizer.vocab_size())
 
-    logits, loss = model(xb, yb)
-    print(logits.shape)
-    print(f"Loss: {loss}")
+    idx = torch.zeros((1, 1), dtype=torch.long)
+    generated_batch = model.generate(idx=idx, max_new_tokens=100)
+    print(tokenizer.decode(generated_batch[0].tolist()))
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+
+    for steps in range(10000):
+        xb, yb = data.training_batch()
+        _, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    print(loss.item())
+
+    idx = torch.zeros((1, 1), dtype=torch.long)
+    generated_batch = model.generate(idx=idx, max_new_tokens=100)
+    print(tokenizer.decode(generated_batch[0].tolist()))
 
 
 if __name__ == "__main__":
