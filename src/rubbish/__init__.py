b659bff98178e9d65bf66c596cbda4a62a1acdad
Markus Klein <markus-klein@live.de>
2025-11-07 19:10:49 +0100

    refactor: Extract model into own module

--- src/rubbish/__init__.py
+++ src/rubbish/__init__.py
@@ -1,48 +1,8 @@
-from typing import override
-
 import torch
-import torch.nn as nn
-from torch.types import Tensor
 
 from .tokenizer import Tokenizer
 from .data import Data
-
-
-class BigramLanguageModel(nn.Module):
-    def __init__(self, vocab_size: int):
-        super().__init__()
-        self.token_embedding_table: nn.Embedding = nn.Embedding(vocab_size, vocab_size)
-
-    @override
-    def forward(
-        self, idx: Tensor, targets: Tensor | None = None
-    ) -> tuple[Tensor, Tensor | None]:
-        logits = self.token_embedding_table(idx)
-
-        if targets is not None:
-            # Optionally calculate loss and reduce dimension of logits
-
-            # Logits have dimension (Batch, Time, Channel)
-            B, T, C = logits.shape
-            logits = logits.view(B * T, C)
-            # Targets has have dimension (Batch, Time)
-            targets = targets.view(B * T)
-            loss = nn.functional.cross_entropy(logits, targets)
-        else:
-            loss = None
-
-        return logits, loss
-
-    def generate(self, idx: Tensor, max_new_tokens: int) -> Tensor:
-        print(idx.shape)
-        print(idx)
-        for _ in range(max_new_tokens):
-            logits, loss = self(idx)
-            logits = logits[:, -1, :]
-            probs = nn.functional.softmax(logits, dim=-1)
-            idx_next = torch.multinomial(probs, num_samples=1)
-            idx = torch.cat((idx, idx_next), dim=1)
-        return idx
+from .model import BigramLanguageModel
 
 
 def main() -> None:
