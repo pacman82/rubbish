a711ad2d3d3750897fd05ba072744e6b9e824d85
Markus Klein <markus-klein@live.de>
2025-11-07 18:24:24 +0100

    refactor: Extract data into module

--- src/rubbish/__init__.py
+++ src/rubbish/__init__.py
@@ -1,39 +1,11 @@
-from typing import final, override
+from typing import override
 
 import torch
 import torch.nn as nn
 from torch.types import Tensor
 
 from .tokenizer import Tokenizer
-
-
-def _batch(
-    data: torch.Tensor, context_size: int, batch_size: int
-) -> tuple[torch.Tensor, torch.Tensor]:
-    assert len(data) >= context_size + 1, "Data too small for the given context size."
-    ix = torch.randint(high=len(data) - context_size, size=(batch_size,))
-    context = torch.stack([data[i : i + context_size] for i in ix])
-    target = torch.stack([data[i + 1 : i + context_size + 1] for i in ix])
-    return context, target
-
-
-@final
-class Data:
-    def __init__(
-        self, text: str, tokenizer: Tokenizer, context_size: int, batch_size: int
-    ):
-        tokenized = torch.tensor(tokenizer.encode(text), dtype=torch.long)
-        n = int(0.9 * len(tokenized))
-        self.training = tokenized[:n]
-        self.validation = tokenized[n:]
-        self.context_size = context_size
-        self.batch_size = batch_size
-
-    def training_batch(self) -> tuple[torch.Tensor, torch.Tensor]:
-        return _batch(self.training, self.context_size, self.batch_size)
-
-    def validation_batch(self) -> tuple[torch.Tensor, torch.Tensor]:
-        return _batch(self.validation, self.context_size, self.batch_size)
+from .data import Data
 
 
 class BigramLanguageModel(nn.Module):
@@ -73,7 +45,7 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 
-def main():
+def main() -> None:
     torch.manual_seed(1337)
 
     with open("input.txt", "r", encoding="utf-8") as f:
