bc01120042b7144795e5c53094b1a3327eadffed
Markus Klein <markus-klein@live.de>
2025-11-12 19:55:48 +0100

    feat: Rust version samples random batches

--- rust/src/data.rs
+++ rust/src/data.rs
@@ -1,53 +1,72 @@
-use anyhow::Context;
 use candle_core::{Device, Tensor};
 use rand::{
     Rng,
     distr::{Distribution, Uniform},
 };
 
-/// Data ready for training and validation.
-pub struct Data {
+/// Samples training and validation batches
+pub struct BatchSampler {
     training_data: Tensor,
     validation_data: Tensor,
+    // We reuse these buffers for composing batches to avoid repeated allocations
+    context_batch_buffer: Vec<Tensor>,
+    target_batch_buffer: Vec<Tensor>,
 }
 
-impl Data {
-    pub fn from_tokenized(tokenized: Vec<u32>, device: &Device) -> anyhow::Result<Self> {
+impl BatchSampler {
+    pub fn from_tokenized(tokenized: Vec<u32>, device: &Device) -> Self {
         let num_tokens = tokenized.len();
         eprintln!("Number of tokens in input data: {num_tokens}");
-        let tokenized = Tensor::from_vec(tokenized, num_tokens, device)
-            .context("Unable to create tokenized data on device")?;
+        let tokenized = Tensor::from_vec(tokenized, num_tokens, device).unwrap();
         let fraction_of_input_used_for_training = 0.9;
         eprintln!("Fraction of input used for training: {fraction_of_input_used_for_training}");
         let num_training_tokens =
             (fraction_of_input_used_for_training * num_tokens as f64).trunc() as usize;
         let num_validation_tokens = num_tokens - num_training_tokens;
-        let training_data = tokenized.narrow(0, 0, num_training_tokens)?;
-        let validation_data = tokenized.narrow(0, num_training_tokens, num_tokens)?;
+        let training_data = tokenized.narrow(0, 0, num_training_tokens).unwrap();
+        let validation_data = tokenized
+            .narrow(0, num_training_tokens, num_validation_tokens)
+            .unwrap();
         eprintln!("Number of training tokens: {num_training_tokens}");
         eprintln!("Number of validation tokens: {num_validation_tokens}");
-        Ok(Data {
+        BatchSampler {
             training_data,
             validation_data,
-        })
+            context_batch_buffer: Vec::new(),
+            target_batch_buffer: Vec::new(),
+        }
     }
 
     pub fn sample_training_batch(
-        &self,
+        &mut self,
         batch_size: usize,
         context_size: usize,
-        rng: &mut impl Rng,
+        rng: &mut impl BatchStartIndices,
     ) -> SampleBatch {
-        sample_batch(&self.training_data, batch_size, context_size, rng)
+        sample_batch(
+            &self.training_data,
+            batch_size,
+            context_size,
+            rng,
+            &mut self.context_batch_buffer,
+            &mut self.target_batch_buffer,
+        )
     }
 
     pub fn sample_validation_batch(
-        &self,
+        &mut self,
         batch_size: usize,
         context_size: usize,
-        rng: &mut impl Rng,
+        rng: &mut impl BatchStartIndices,
     ) -> SampleBatch {
-        sample_batch(&self.validation_data, batch_size, context_size, rng)
+        sample_batch(
+            &self.validation_data,
+            batch_size,
+            context_size,
+            rng,
+            &mut self.context_batch_buffer,
+            &mut self.target_batch_buffer,
+        )
     }
 }
 
@@ -55,29 +74,124 @@ fn sample_batch(
     data: &Tensor,
     batch_size: usize,
     context_size: usize,
-    rng: &mut impl Rng,
+    rng: &mut impl BatchStartIndices,
+    context_batch_buffer: &mut Vec<Tensor>,
+    target_batch_buffer: &mut Vec<Tensor>,
 ) -> SampleBatch {
     // We need to be able to sample context size tokens and one additional target tokens
-    assert!(data.dim(0).unwrap() >= context_size + 1);
+    assert!(
+        data.dim(0).unwrap() >= context_size + 1,
+        "Not enough data is available to suppor requested batch size"
+    );
+
+    context_batch_buffer.clear();
+    target_batch_buffer.clear();
 
     let up = data.dim(0).unwrap() - context_size;
-    let random_start_indices = Uniform::new(0, up).unwrap();
-    let mut context_batch = Vec::with_capacity(batch_size);
-    let mut target_batch = Vec::with_capacity(batch_size);
     for _ in 0..batch_size {
-        let start_index = random_start_indices.sample(rng);
-        context_batch.push(data.narrow(0, start_index, context_size).unwrap());
-        target_batch.push(data.narrow(0, start_index + context_size, 1).unwrap());
+        let start_index = rng.next_batch_start_index(up);
+        context_batch_buffer.push(data.narrow(0, start_index, context_size).unwrap());
+        target_batch_buffer.push(data.narrow(0, start_index + context_size, 1).unwrap());
     }
-    let context = Tensor::stack(context_batch.as_slice(), 0).unwrap();
-    let target = Tensor::stack(target_batch.as_slice(), 0).unwrap();
+    let context = Tensor::stack(context_batch_buffer.as_slice(), 0).unwrap();
+    let target = Tensor::stack(target_batch_buffer.as_slice(), 0).unwrap();
     SampleBatch { context, target }
 }
 
+/// A batch sampled for training
 pub struct SampleBatch {
-    context: Tensor,
-    target: Tensor,
+    /// A tensor of shape [batch_size, context_size]. Each line in the batch dimension contains
+    /// context_size contiguous tokens from the training data.
+    pub context: Tensor,
+    /// A tensor of shape [batch_size, 1]. Each line in the batch dimension contains the token
+    /// immediately following the context tokens, of the batch with the same index of context.
+    pub target: Tensor,
+}
+
+/// Provide start indices for training or validation batches. This trait is auto implemented for any
+/// `Rng` as we intend to sample the batches randomly in training.
+pub trait BatchStartIndices {
+    /// # Parameters
+    ///
+    /// * `up`: Exclusive upper bound for the generated index.
+    fn next_batch_start_index(&mut self, up: usize) -> usize;
+}
+
+impl<T> BatchStartIndices for T
+where
+    T: Rng,
+{
+    fn next_batch_start_index(&mut self, up: usize) -> usize {
+        Uniform::new(0, up).unwrap().sample(self)
+    }
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::device::choose_device;
+
+    use super::*;
+
+    #[test]
+    fn sample_training_batch() {
+        let device = choose_device();
+        let batch_size = 3;
+        let context_size = 2;
+        let mut rng = CountingStartIndices::new();
+
+        let mut data = BatchSampler::from_tokenized((0..10).collect(), &device);
+        let batch = data.sample_training_batch(batch_size, context_size, &mut rng);
+        assert_eq!(batch.context.dims(), &[3, 2]);
+        assert_eq!(batch.target.dims(), &[3, 1]);
+
+        assert_eq!(
+            vec![vec![0, 1], vec![1, 2], vec![2, 3]],
+            batch.context.to_vec2::<u32>().unwrap()
+        );
+
+        assert_eq!(
+            vec![vec![2], vec![3], vec![4]],
+            batch.target.to_vec2::<u32>().unwrap()
+        );
+    }
+
+    #[test]
+    fn sample_validation_batch() {
+        let device = choose_device();
+        let batch_size = 3;
+        let context_size = 2;
+        let mut rng = CountingStartIndices::new();
+
+        let mut data = BatchSampler::from_tokenized((0..30).collect(), &device);
+        let batch = data.sample_validation_batch(batch_size, context_size, &mut rng);
+        assert_eq!(batch.context.dims(), &[3, 2]);
+        assert_eq!(batch.target.dims(), &[3, 1]);
+
+        assert_eq!(
+            vec![vec![27, 28], vec![27, 28], vec![27, 28]],
+            batch.context.to_vec2::<u32>().unwrap()
+        );
+
+        assert_eq!(
+            vec![vec![29], vec![29], vec![29]],
+            batch.target.to_vec2::<u32>().unwrap()
+        );
+    }
+
+    /// Fake random start indices for testing purposes. Counts batch indices starting from 0.
+    struct CountingStartIndices(usize);
+
+    impl CountingStartIndices {
+        fn new() -> Self {
+            CountingStartIndices(0)
+        }
+    }
+
+    impl BatchStartIndices for CountingStartIndices {
+        fn next_batch_start_index(&mut self, up: usize) -> usize {
+            let value = self.0 % up;
+            self.0 += 1;
+            value
+        }
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,10 +5,14 @@ mod tokenizer;
 use std::fs;
 
 use anyhow::Context;
+use rand::rng;
 
 use crate::device::choose_device;
 
-use self::{data::Data, tokenizer::Tokenizer};
+use self::{data::BatchSampler, tokenizer::Tokenizer};
+
+const BATCH_SIZE: usize = 4;
+const CONTEXT_SIZE: usize = 8;
 
 fn main() -> anyhow::Result<()> {
     let device = choose_device();
@@ -17,7 +21,50 @@ fn main() -> anyhow::Result<()> {
     let tokenizer = Tokenizer::from_text(&text);
     let tokenized = tokenizer.encode(&text);
 
-    let data = Data::from_tokenized(tokenized, &device);
+    let mut batch_sampler = BatchSampler::from_tokenized(tokenized, &device);
+
+    // Sample a training and a validation batch and print their decoded versions
+    let rng = &mut rng();
+    let training_batch = batch_sampler.sample_training_batch(BATCH_SIZE, CONTEXT_SIZE, rng);
+    let validation_batch = batch_sampler.sample_validation_batch(BATCH_SIZE, CONTEXT_SIZE, rng);
+
+    println!("Training batch:");
+    for batch_index in 0..BATCH_SIZE {
+        let data = training_batch
+            .context
+            .get(batch_index)
+            .unwrap()
+            .to_vec1()
+            .unwrap();
+        let context = tokenizer.decode(&data);
+        let data = training_batch
+            .target
+            .get(batch_index)
+            .unwrap()
+            .to_vec1()
+            .unwrap();
+        let target = tokenizer.decode(&data);
+        println!("context: {context}, target: {target}");
+    }
+
+    println!("Validation batch:");
+    for batch_index in 0..BATCH_SIZE {
+        let data = validation_batch
+            .context
+            .get(batch_index)
+            .unwrap()
+            .to_vec1()
+            .unwrap();
+        let context = tokenizer.decode(&data);
+        let data = validation_batch
+            .target
+            .get(batch_index)
+            .unwrap()
+            .to_vec1()
+            .unwrap();
+        let target = tokenizer.decode(&data);
+        println!("context: {context}, target: {target}");
+    }
 
     Ok(())
 }
