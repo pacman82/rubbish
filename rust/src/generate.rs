d4b5d1fe2dfc2c3c95d2d868569443cf055fef97
Markus Klein <markus-klein@live.de>
2025-11-20 15:35:20 +0100

    test: Test for model emitting vocab size

--- rust/src/generate.rs
+++ rust/src/generate.rs
@@ -1,7 +1,52 @@
 use candle_core::{Device, Tensor};
+use candle_nn::ModuleT;
 use candle_transformers::generation::LogitsProcessor;
 
-use crate::model::PredictLogits;
+use crate::{CONTEXT_SIZE, model::DeviceAffine};
+
+/// Predict logits continuing the input sequence. Used for text generation. This is the step before
+/// sampling.
+pub trait PredictLogits: DeviceAffine {
+    /// # Parameters
+    ///
+    /// Use the model to predict the logits for the next token for each batch in a sequence of
+    /// inputs.
+    ///
+    /// * `xs`: Input tensor of shape (batch_size, sequence_length). The sequence length must not
+    ///   exceed the context size of the model.
+    ///
+    /// # Returns
+    ///
+    /// Tensor of shape (batch_size, vocab_size) representing the logits for the next token.
+    fn predict_logits(&self, xs: &Tensor) -> Tensor;
+
+    /// Context size of the model. Used to limit the `sequence_length` passed into
+    /// [`PredictLogits::predict_logits`]
+    fn context_size(&self) -> usize;
+}
+
+impl<T> PredictLogits for T
+where
+    T: ModuleT + DeviceAffine,
+{
+    fn predict_logits(&self, xs: &Tensor) -> Tensor {
+        // Forward pass through the model
+        let logits = self.forward_t(xs, false).unwrap();
+
+        // Get the last time step's embeddings
+        let (_batch, time_size, _vocab) = logits.dims3().unwrap();
+        let last_logits = logits.get_on_dim(1, time_size - 1).unwrap();
+        let last_logits = last_logits.squeeze(0).unwrap();
+
+        // For simplicity, we will just return the last embeddings as logits
+        // In a real model, you would have a linear layer here to project to vocab size
+        last_logits
+    }
+
+    fn context_size(&self) -> usize {
+        CONTEXT_SIZE
+    }
+}
 
 pub struct GenerateIter<'a, M, S> {
     model: &'a M,
@@ -63,12 +108,13 @@ mod tests {
     use std::{mem::replace, sync::Mutex};
 
     use candle_core::Tensor;
+    use candle_nn::ModuleT;
     use candle_transformers::generation::{LogitsProcessor, Sampling};
 
     use crate::{
         device::choose_device,
-        generate::{GenerateIter, Sampler},
-        model::{DeviceAffine, PredictLogits},
+        generate::{GenerateIter, PredictLogits, Sampler},
+        model::DeviceAffine,
     };
 
     #[test]
@@ -165,5 +211,39 @@ mod tests {
             self.current += 1;
             next
         }
+    }
+
+    #[test]
+    fn predict_logits_returns_last_time_dimension_of_forward_pass() {
+        // Given
+        struct ModelStub;
+
+        impl ModuleT for ModelStub {
+            /// Returns
+            /// [[0 1 2], [3 4 5], [6 7 8], [9 10 11]],
+            /// [[12 13 14], [15 16 17], [18 19 20], [21 22 23]]
+            fn forward_t(&self, _xs: &Tensor, _train: bool) -> candle_core::Result<Tensor> {
+                // Batch size 2, time size 4, vocab size 3
+                let vec = (0..(2 * 4 * 3)).map(|x| x as f32).collect::<Vec<f32>>();
+                Tensor::from_slice(&vec, (2, 4, 3), &self.device())
+            }
+        }
+
+        impl DeviceAffine for ModelStub {
+            fn device(&self) -> candle_core::Device {
+                choose_device()
+            }
+        }
+
+        // When
+        let dummy_input = &Tensor::from_vec(vec![0u32, 0u32], 1, &choose_device()).unwrap();
+        let logits = ModelStub.predict_logits(dummy_input);
+
+        // Then
+        let logits_first_batch = logits.get_on_dim(0, 0).unwrap().to_vec1::<f32>().unwrap();
+        assert_eq!([9f32, 10., 11.].as_slice(), logits_first_batch);
+
+        let logits_second_batch = logits.get_on_dim(0, 1).unwrap().to_vec1::<f32>().unwrap();
+        assert_eq!([21f32, 22., 23.].as_slice(), logits_second_batch);
     }
 }
--- rust/src/model.rs
+++ rust/src/model.rs
@@ -68,6 +68,7 @@ impl ModuleT for Model {
             .unwrap();
         let x = (token_embeddings + positional_embeddings).unwrap();
         // We use a linear layer to map the channel back to the vocabulary size.
+        // I.e Batch, Time, Embedding size -> Batch, Time, Vocab Size
         let logits = self.lm_head.forward_t(&x, train).unwrap();
         Ok(logits)
     }
@@ -85,50 +86,6 @@ impl DeviceAffine for Model {
     }
 }
 
-/// Predict logits continuing the input sequence. Used for text generation. This is the step before
-/// sampling.
-pub trait PredictLogits: DeviceAffine {
-    /// # Parameters
-    ///
-    /// Use the model to predict the logits for the next token for each batch in a sequence of
-    /// inputs.
-    ///
-    /// * `xs`: Input tensor of shape (batch_size, sequence_length). The sequence length must not
-    ///   exceed the context size of the model.
-    ///
-    /// # Returns
-    ///
-    /// Tensor of shape (batch_size, vocab_size) representing the logits for the next token.
-    fn predict_logits(&self, xs: &Tensor) -> Tensor;
-
-    /// Context size of the model. Used to limit the `sequence_length` passed into
-    /// [`PredictLogits::predict_logits`]
-    fn context_size(&self) -> usize;
-}
-
-impl<T> PredictLogits for T
-where
-    T: ModuleT + DeviceAffine,
-{
-    fn predict_logits(&self, xs: &Tensor) -> Tensor {
-        // Forward pass through the model
-        let logits = self.forward_t(xs, false).unwrap();
-
-        // Get the last time step's embeddings
-        let (_batch, time_size, _vocab) = logits.dims3().unwrap();
-        let last_logits = logits.get_on_dim(1, time_size - 1).unwrap();
-        let last_logits = last_logits.squeeze(0).unwrap();
-
-        // For simplicity, we will just return the last embeddings as logits
-        // In a real model, you would have a linear layer here to project to vocab size
-        last_logits
-    }
-
-    fn context_size(&self) -> usize {
-        CONTEXT_SIZE
-    }
-}
-
 impl Trainable for Model {
     fn all_vars(&self) -> Vec<Var> {
         self.var_map.all_vars()
@@ -137,46 +94,24 @@ impl Trainable for Model {
 
 #[cfg(test)]
 mod tests {
-    use candle_core::{Device, Tensor};
+    use candle_core::{DType, Device, Tensor};
     use candle_nn::ModuleT;
 
-    use crate::{
-        device::choose_device,
-        model::{DeviceAffine, PredictLogits},
-    };
+    use crate::{device::choose_device, model::Model};
 
+    // This tests drives the linear layer, mapping back to vocab size
     #[test]
-    fn predict_logits_returns_last_time_dimension_of_forward_pass() {
-        // Given
-        struct ModelStub;
+    fn logits_have_dim_batch_time_vocab_size() {
+        let device = choose_device();
+        let vocab_size = 5;
+        let time = 1;
+        let batch_size = 2;
+        let tensor = Tensor::zeros((batch_size, time), DType::U32, &device).unwrap();
+        let model = Model::new(device, vocab_size);
 
-        impl ModuleT for ModelStub {
-            /// Returns
-            /// [[0 1 2], [3 4 5], [6 7 8], [9 10 11]],
-            /// [[12 13 14], [15 16 17], [18 19 20], [21 22 23]]
-            fn forward_t(&self, _xs: &Tensor, _train: bool) -> candle_core::Result<Tensor> {
-                // Batch size 2, time size 4, vocab size 3
-                let vec = (0..(2 * 4 * 3)).map(|x| x as f32).collect::<Vec<f32>>();
-                Tensor::from_slice(&vec, (2, 4, 3), &self.device())
-            }
-        }
+        let logit = model.forward_t(&tensor, false).unwrap();
 
-        impl DeviceAffine for ModelStub {
-            fn device(&self) -> candle_core::Device {
-                choose_device()
-            }
-        }
-
-        // When
-        let dummy_input = &Tensor::from_vec(vec![0u32, 0u32], 1, &choose_device()).unwrap();
-        let logits = ModelStub.predict_logits(dummy_input);
-
-        // Then
-        let logits_first_batch = logits.get_on_dim(0, 0).unwrap().to_vec1::<f32>().unwrap();
-        assert_eq!([9f32, 10., 11.].as_slice(), logits_first_batch);
-
-        let logits_second_batch = logits.get_on_dim(0, 1).unwrap().to_vec1::<f32>().unwrap();
-        assert_eq!([21f32, 22., 23.].as_slice(), logits_second_batch);
+        assert_eq!((batch_size, time, vocab_size), logit.dims3().unwrap());
     }
 
     #[test]
